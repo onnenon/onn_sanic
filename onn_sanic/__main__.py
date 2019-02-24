0a9606cd70d7a03a742c1677687c8d91cebce9d4
Stephen Onnen <stephen.onnen@gmail.com>
2019-02-23 19:39:46 -0600

    Added websocket route

--- onn_sanic/__main__.py
+++ onn_sanic/__main__.py
@@ -1,6 +1,9 @@
 from sanic import Sanic, response
 
 from onn_sanic.api import register_blueprints
+from sanic.websocket import WebSocketProtocol
+from sanic.log import logger
+from websockets import ConnectionClosed
 
 app = Sanic(__name__)
 
@@ -14,5 +17,25 @@ async def index(request):
     return await response.file("./onn_sanic/static/html/index.html")
 
 
+sockets = []
+
+
+@app.websocket("/chat")
+async def chat(request, ws):
+    sockets.append(ws)
+
+    while not ws.closed:
+        message = await ws.recv()
+        logger.debug({"Message": message})
+        for socket in sockets:
+            try:
+                if socket != ws:
+                    await socket.send(message)
+            except ConnectionClosed:
+                sockets.remove(socket)
+
+    sockets.remove(ws)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, protocol=WebSocketProtocol)
--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     name="onn_sanic",
     version="0.0.1",
     author="Stephen Onnen",
+    author_email="stephen.onnen@gmail.com",
     description="Project site for Stephen Onnen",
     license="MIT",
     url="https://github.com/onnenon/onn_sanic",
+    install_requires=["sanic"],
     packages=find_packages(),
 )
